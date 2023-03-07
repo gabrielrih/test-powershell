aed5be82886d9683c0e110c97e6f112d40e4ecb7
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2023-03-07 09:38:56 -0300

    Add a list on the config script

--- simple-config-file/main.ps1
+++ simple-config-file/main.ps1
@@ -1,6 +1,13 @@
 # Reference: https://kyleparrish.com/blog/powershell-script-config-file/
 $ConfigFileName = "config.conf"
 
-Foreach ($line in $(Get-Content $ConfigFileName)) {
-    Write-Output $line
+# It navegates through the lines on the config file and add its value in a list
+$IPList = New-Object -TypeName 'System.Collections.ArrayList';
+foreach ($line in $(Get-Content $ConfigFileName)) {
+    $IPList.Add($line) > $null
+}
+
+# It prints all the list elements
+foreach ($CurrentIp In $IPList) {
+    Write-Output "This is the list value: $CurrentIp"
 }
